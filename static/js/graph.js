1cce3244668853dde10b5d6cb4fada215c8ca3e5
Will <86987398+Will6855@users.noreply.github.com>
2023-03-06 18:14:19 +0100

    Adding variable to pie chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,10 +1,9 @@
 const ctx = document.getElementById('myChart');
 
-const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+data = {
+    labels: ['Number of Users', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [nbUsers, 19, 3, 5, 2, 3],
       borderWidth: 1
     }]
   }
