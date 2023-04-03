8207cc755e46fa05b7d73ef384bf22a6e4bdbbe4
Will <86987398+Will6855@users.noreply.github.com>
2023-04-03 21:03:43 +0200

    Update

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+# Créer la connexion à la BDD
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -9,6 +10,7 @@ def get_db_connection():
 
 app = Flask(__name__)
 
+# Fonction à executer sur la racine
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -16,13 +18,15 @@ def index():
     conn.close()
     return render_template('index.html', nbUsers=nbUsers)
 
+# Fonction à executer sur /users
 @app.route('/users')
 def users():
     conn = get_db_connection()
-    users = conn.execute('SELECT * FROM users').fetchall()
+    users = conn.execute('SELECT * FROM users').fetchall() # Récupére tous les utilisateurs de la BDD
     conn.close()
-    return render_template('users.html', users=users)
+    return render_template('users.html', users=users) # Affiche les utilisateurs
 
+# Fonction à executer sur /CreateUser
 @app.route('/createUser', methods=('GET', 'POST'))
 def createUser():
     if request.method == 'POST':
@@ -37,13 +41,14 @@ def createUser():
             flash('Password is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO users (username, password) VALUES (?, ?)',
+            conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', # Insere un nouvel utilisateur dans la BDD
                          (username , password))
             conn.commit()
             conn.close()
-            return redirect(url_for('users'))
+            return redirect(url_for('users')) # Retourne sur la page affichant tous les utilisateurs
     return render_template('createUser.html')
 
+# Fonction servant à récupérer toutes les informations d'un utilisateur à partir de son ID
 def get_user(user_id):
     conn = get_db_connection()
     user = conn.execute('SELECT * FROM users WHERE id = ?',
@@ -53,6 +58,7 @@ def get_user(user_id):
         abort(404)
     return user
 
+# Fonction permettant de modifier les informations d'un utilisateur à partir de son ID
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
     user = get_user(id)
@@ -71,26 +77,26 @@ def edit(id):
             conn = get_db_connection()
             conn.execute('UPDATE users SET username = ?, password = ?'
                          ' WHERE id = ?',
-                         (username, password, id))
+                         (username, password, id)) # Modifie les informations de l'utilisateurs
             conn.commit()
             conn.close()
             return redirect(url_for('users'))
 
     return render_template('editUser.html', user=user)
 
+# Fonction permettant de supprimer un utilisateur à partir de son ID
 @app.route('/<int:id>/delete', methods=('GET', 'POST',))
 def delete(id):
     user = get_user(id)
     conn = get_db_connection()
-    conn.execute('DELETE FROM users WHERE id = ?', (id,))
+    conn.execute('DELETE FROM users WHERE id = ?', (id,)) # Supprime l'utilisateur
     conn.commit()
     conn.close()
     flash('"{}" was successfully deleted!'.format(user['username']))
     return redirect(url_for('users'))
 
 
-
-
+# Fonction qui récupérent un ensemble d'informations pour l'afficher sur la page WEB
 @app.route('/dashboard')
 def dashboard():
     conn = get_db_connection()
